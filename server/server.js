e53514c876259ec84c7d6f246091f9d546e13297
veeda_241 <vyas.sk17@gmail.com>
2025-08-13 13:23:52 +0530

    intial 1.1

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/hackathons', require('./routes/hackathons'));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
